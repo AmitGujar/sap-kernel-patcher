b6f01fa33f89887fe2b4e9a7233dadb75a5c3515
AmitGujar <amithero3342@gmail.com>
2023-10-30 16:54:19 +0530

    dynamic values added

--- upgrade.sh
+++ upgrade.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+source $HOME/kernelPlaybooks/alert.sh
+
 start_upgrade() {
     ansible-playbook -i config/inventory playbooks/main.yaml
     if [ $? -ne 0 ]; then
-        echo "Failed to upgrade the kernal"
+        echo "Check the input file or directory name"
         exit 1
-    else
-        ansible-playbook -i config/inventory playbooks/hdb.yaml
-        if [ $? -ne 0 ]; then
-            echo "Failed to start the HDB database"
-            exit 1
-        else
-            ansible-playbook -i config/inventory playbooks/services.yaml
-            if [ $? -ne 0 ]; then
-                echo "Failed to start the stopped services"
-                exit 1
-            else
-                echo "..."
-            fi
-        fi
+    # else
+    #     ansible-playbook -i config/inventory playbooks/hdb.yaml
+    #     if [ $? -ne 0 ]; then
+    #         echo "Failed to start the HDB database"
+    #         exit 1
+    #     else
+    #         ansible-playbook -i config/inventory playbooks/services.yaml
+    #         if [ $? -ne 0 ]; then
+    #             echo "Failed to start the stopped services"
+    #             exit 1
+    #         else
+    #             echo "..."
+    #         fi
+    #     fi
     fi
     echo "Kernel is upgraded on target machine"
+    set_alert "Kernel upgrade is successful ✅"
 }
 start_upgrade
